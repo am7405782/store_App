4c9468130efbb28e5a231abc34d60a099fb60931
Ahmed abdelnadi <93190015+am7405782@users.noreply.github.com>
2023-10-20 05:05:21 -0700

    onboarding Screen

---
+++ lib/Screen/Onbording/OnbordingScrren.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnbordingScrren extends StatelessWidget {
+  const OnbordingScrren({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
